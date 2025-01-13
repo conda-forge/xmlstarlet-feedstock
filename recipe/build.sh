42df5aadd1f1e60a7516279c5f7680628301deda
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-13 00:19:28 -0800

    fix unbound variable

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
 autoreconf --force --verbose --install
 ./configure --prefix=${PREFIX}
 make
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
 make tests
 fi
 make install
