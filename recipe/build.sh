c4c97a80f0d1a03f60ad995dc23201c5507d3fbf
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-12 23:20:42 -0800

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
+set -o xtrace -o nounset -o pipefail -o errexit
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
+
 ./configure --prefix=${PREFIX}
 make
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
 make tests
+fi
 make install
