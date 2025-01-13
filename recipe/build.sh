6a21d35ae46f3e5ab50a249f0d5976fc209b6192
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-12 23:30:45 -0800

    enable osx-arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
+export CFLAGS="${CFLAGS} -Wincompatible-function-pointer-types"
 ./configure --prefix=${PREFIX}
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
