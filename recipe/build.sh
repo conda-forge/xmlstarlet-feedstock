91c0a0779925a197087f2bab24180130e33fb388
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-12 23:45:46 -0800

    fix CFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 ./configure --prefix=${PREFIX}
-export CFLAGS="${CFLAGS} -Wincompatible-function-pointer-types"
+export CFLAGS="${CFLAGS} -Wno-incompatible-function-pointer-types"
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
 make tests
